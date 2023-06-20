63fc91b62f0dfcc1bf2fc728806e5572292a45fc
Yann <yann.leguilly@gmail.com>
2023-06-20 14:59:38 +0900

    fleek: apply

--- shell.nix
+++ shell.nix
@@ -1,5 +1,16 @@
 { pkgs, misc, ... }: {
-  # DO NOT EDIT: This file is managed by fleek. Manual changes will be overwritten.# zsh
+  # DO NOT EDIT: This file is managed by fleek. Manual changes will be overwritten.
+    programs.exa.enableAliases = true;
+    
+    programs.exa.extraOptions = [
+   "--group-directories-first"
+   "--header"
+];
+    
+    programs.bat.config = {
+  theme = "TwoDark";
+};
+    # zsh
   programs.zsh.profileExtra = "[ -r ~/.nix-profile/etc/profile.d/nix.sh ] && source  ~/.nix-profile/etc/profile.d/nix.sh";
   programs.zsh.enableCompletion = true;
   programs.zsh.enable = true;
